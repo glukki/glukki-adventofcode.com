a344e3ff1af7c1cd5057f978ccd7ef5aa3414e6e
Vitaliy (glukki) Meshchaninov <glukki.spb.ru@gmail.com>
2019-12-05 16:12:55 +0300

    Day 4: Secure Container, Part Two

---
+++ src/2019/0402.js
@@ -0,0 +1,27 @@
+const tests = [
+  (digits) => (digits[0] === digits[1] && digits[1] !== digits[2]) ||
+    (digits[0] !== digits[1] && digits[1] === digits[2] && digits[2] !== digits[3]) ||
+    (digits[1] !== digits[2] && digits[2] === digits[3] && digits[3] !== digits[4]) ||
+    (digits[2] !== digits[3] && digits[3] === digits[4] && digits[4] !== digits[5]) ||
+    (digits[3] !== digits[4] && digits[4] === digits[5]),
+  (digits) => digits[0] <= digits[1] &&
+    digits[1] <= digits[2] &&
+    digits[2] <= digits[3] &&
+    digits[3] <= digits[4] &&
+    digits[4] <= digits[5],
+]
+
+const run = range => {
+  const [min, max] = range.split('-')
+    .map(Number)
+
+  let found = 0
+  for (let i = min; i <= max && i <= 999999; i++) {
+    const digits = String(i)
+    found += tests.every(test => test(digits))
+  }
+
+  return found
+}
+
+console.log(run(process.argv[2]))
