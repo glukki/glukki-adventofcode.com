daa752658632fde211e1aeaaedd14bd0a1744b85
Vitaliy (glukki) Meshchaninov <glukki.spb.ru@gmail.com>
2022-12-05 01:04:36 +0400

    2022 Day 2: Rock Paper Scissors, Part Two

---
+++ src/2022/0202.ts
@@ -0,0 +1,39 @@
+const decoder = new TextDecoder();
+
+let input = "";
+for await (const chunk of Deno.stdin.readable) {
+  input += decoder.decode(chunk);
+}
+
+const lookup = {
+  A: { // Rock
+    X: 0 + 3, // Lose
+    Y: 3 + 1, // Draw
+    Z: 6 + 2, // Win
+  },
+  B: { // Paper
+    X: 0 + 1, // Lose
+    Y: 3 + 2, // Draw
+    Z: 6 + 3, // Win
+  },
+  C: { // Scissors
+    X: 0 + 2, // Lose
+    Y: 3 + 3, // Draw
+    Z: 6 + 1, // Win
+  },
+};
+
+const result = input.split("\n")
+  .map((line) => {
+    const [opponent, , you] = line;
+    const score = lookup[opponent as "A" | "B" | "C"][you as "X" | "Y" | "Z"];
+
+    if (typeof score !== "number") {
+      throw new Error("Unknown character:" + line);
+    }
+
+    return score;
+  })
+  .reduce((memo, score) => memo + score, 0);
+
+console.log(result);
