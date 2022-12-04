d923f00cd89eaa873c67de444d860381fcfebdd4
Vitaliy (glukki) Meshchaninov <glukki.spb.ru@gmail.com>
2022-12-05 01:56:00 +0400

    2022 Day 3: Rucksack Reorganization, Part Two

---
+++ src/2022/0302.ts
@@ -0,0 +1,45 @@
+const decoder = new TextDecoder();
+
+let input = "";
+for await (const chunk of Deno.stdin.readable) {
+  input += decoder.decode(chunk);
+}
+
+const typeLookup = new Map();
+for (let i = 97; i < 123; i++) { // a=97, z=122
+  typeLookup.set(String.fromCharCode(i), i - 96);
+}
+for (let i = 65; i < 91; i++) { // A=65, Z=90
+  typeLookup.set(String.fromCharCode(i), i - 38);
+}
+
+const result = input.split("\n")
+  .reduce((memo, line, idx) => { // groups of 3 lines
+    if (idx % 3 === 0) {
+      memo.push([]);
+    }
+    memo[memo.length - 1].push(line);
+    return memo;
+  }, [] as unknown as string[][])
+  .map((lines) => {
+    const shared = new Set(lines[0]);
+
+    for (let i = 1; i < lines.length; i++) {
+      const backpack = new Set(lines[i]);
+      for (const item of shared.values()) {
+        if (!backpack.has(item)) {
+          shared.delete(item);
+        }
+      }
+    }
+
+    if (shared.size > 1) {
+      throw new Error("Too big shared" + JSON.stringify(lines));
+    }
+
+    return shared.values().next().value;
+  })
+  .map((item) => typeLookup.get(item))
+  .reduce((memo, value) => memo + value, 0);
+
+console.log(result);
