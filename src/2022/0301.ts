1425069ead9397a930d103e1cc444e443ec1814a
Vitaliy (glukki) Meshchaninov <glukki.spb.ru@gmail.com>
2022-12-05 01:32:35 +0400

    2022 Day 3: Rucksack Reorganization

---
+++ src/2022/0301.ts
@@ -0,0 +1,26 @@
+const decoder = new TextDecoder();
+
+let input = "";
+for await (const chunk of Deno.stdin.readable) {
+  input += decoder.decode(chunk);
+}
+
+const typeLookup = new Map();
+for (let i = 97; i < 123; i++) { // a=97, z=122
+  typeLookup.set(String.fromCharCode(i), i - 96);
+}
+for (let i = 65; i < 91; i++) { // A=65, Z=90
+  typeLookup.set(String.fromCharCode(i), i - 38);
+}
+
+const result = input.split("\n")
+  .map((line) => {
+    const lookup = new Set(line.slice(0, line.length / 2)); // left
+    return line.slice(line.length / 2) // right
+      .split("")
+      .find((char) => lookup.has(char));
+  })
+  .map((char) => typeLookup.get(char))
+  .reduce((memo, score) => memo + score, 0);
+
+console.log(result);
