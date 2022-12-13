7fe85a387284718fa56646b97d70995f88f616a0
Vitaliy (glukki) Meshchaninov <glukki.spb.ru@gmail.com>
2022-12-13 18:12:58 +0400

    2022 Day 5: Supply Stacks, Part Two

---
+++ src/2022/0502.ts
@@ -0,0 +1,50 @@
+const decoder = new TextDecoder();
+
+let input = "";
+for await (const chunk of Deno.stdin.readable) {
+  input += decoder.decode(chunk);
+}
+
+const inputSections = input.split("\n\n");
+
+const stacks = inputSections[0].split("\n")
+  .reverse()
+  .reduce((memo, line, idx) => {
+    for (let i = 0; (i * 4 + 1) < line.length; i++) {
+      if (idx === 0) {
+        memo.push([]);
+        continue;
+      }
+
+      const cargo = line[1 + i * 4];
+      if (cargo && cargo !== " ") {
+        memo[i].push(cargo);
+      }
+    }
+
+    return memo;
+  }, [] as string[][]);
+
+const instructions = inputSections[1].split("\n")
+  .map((line) => {
+    const [, count, , source, , destination] = line.split(" ");
+    return {
+      source: Number(source) - 1,
+      destination: Number(destination) - 1,
+      count: Number(count),
+    };
+  });
+
+const result = instructions.reduce((memo, instruction) => {
+  const substack = memo[instruction.source].splice(
+    -instruction.count,
+    instruction.count,
+  );
+  memo[instruction.destination].push(...substack);
+
+  return memo;
+}, stacks)
+  .map((stack) => stack.slice(-1)[0] || " ")
+  .join("");
+
+console.log(result);
